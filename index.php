26eec629506fba7e2700f0724d4c35226774c862
Steven Rivera <ghostszmusic@gmail.com>
2016-12-26 10:47:59 -0500

    index.php and iml

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Ghost Analytics
+Plugin URI: http://GhostszMusic.com
+Description: Plugin for displaying your google analytics
+Author: Steven "Ghost" Rivera
+Version: 1.0
+Author URI: http://GhostszMusic.com
+*/
+
