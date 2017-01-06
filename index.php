119f880aa1b200c4d483fc96b7d34abdcd6115ef
Steven Rivera <ghostszmusic@gmail.com>
2017-01-06 07:21:37 -0500

    ger

--- index.php
+++ index.php
@@ -4,7 +4,20 @@ Plugin Name: Ghost Analytics
 Plugin URI: http://GhostszMusic.com
 Description: Plugin for displaying your google analytics
 Author: Steven "Ghost" Rivera
-Version: 1.0
+Version: 0.1
 Author URI: http://GhostszMusic.com
 */
 
+add_action( 'admin_init', 'ghs_admin_menu' );
+
+function ghs_admin_menu() {
+    add_menu_page(
+        __( 'Analytic', 'textdomain' ),
+        'Analytic',
+        'manage_options',
+        plugins_url(),
+        'analytic_page',
+        '',
+        6
+    );
+}
---
+++ pages/analytics-page.php
@@ -0,0 +1,12 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <title>GHS Analytic Page</title>
+    </head>
+    <body>
+        <h2>GHS Analytic Page</h2>
+        <p>Hey</p>
+    </body>
+
+</html>
+
